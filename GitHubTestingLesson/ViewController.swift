fa232914e2fa660c0d826d69b90a457e15221d0e
Nicole <nicole.2021.nh@gmail.com>
2021-07-20 12:16:53 -0500

    Initial Commit

---
+++ GitHubTestingLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTestingLesson
+//
+//  Created by Nicole hernandez on 7/20/21.
+//  Copyright © 2021 Nicole hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
